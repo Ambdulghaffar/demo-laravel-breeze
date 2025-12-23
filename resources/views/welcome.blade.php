bda2418da493c6a7dbd5f349caf727c5ef5db027
Ambdulghaffar <elhaffarahamadi@gmail.com>
2025-12-23 01:41:04 +0100

    feat: improve welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,9 +20,7 @@
             <nav aria-label="Global" class="flex items-center justify-between p-6 lg:px-8">
                 <div class="flex lg:flex-1">
                     <a href="#" class="-m-1.5 p-1.5">
-                        <span class="sr-only">Your Company</span>
-                        <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                            alt="" class="h-8 w-auto" />
+                        <span class="font-bold text-5xl">FS-Masters</span>
                     </a>
                 </div>
                 <div class="flex lg:hidden">
@@ -36,14 +34,16 @@
                         </svg>
                     </button>
                 </div>
-                <div class="hidden lg:flex lg:gap-x-12">
-                    <a href="#" class="text-sm/6 font-semibold text-gray-900">Product</a>
-                    <a href="#" class="text-sm/6 font-semibold text-gray-900">Features</a>
-                    <a href="#" class="text-sm/6 font-semibold text-gray-900">Marketplace</a>
-                    <a href="#" class="text-sm/6 font-semibold text-gray-900">Company</a>
+                <div class="hidden lg:flex lg:gap-x-12 text-3xl font-semibold text-gray-900">
+                    <a href="#" class="">Acceuil</a>
+                    <a href="#" class="">Actualités</a>
+                    <a href="#" class="">Masters</a>
+                    <a href="#" class="">
+                        À propos
+                    </a>
                 </div>
                 <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-                    <a href="#" class="text-sm/6 font-semibold text-gray-900">Log in <span
+                    <a href="#" class="text-2xl font-semibold text-gray-900">Se connecter<span
                             aria-hidden="true">&rarr;</span></a>
                 </div>
             </nav>
